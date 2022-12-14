ad50fb8f4474d86a7da476eba7c081e86ceeb6f9
AAT <aat@netcompany.com>
2022-12-14 16:06:58 +0100

    Started on 07

---
+++ TGC.AdventOfCode2022.Tests/Runner07Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using TGC.AdventOfCode2022.Runner.Runners;
+using Xunit;
+
+namespace TGC.AdventOfCode2022.Tests;
+
+public class Runner07Tests
+{
+    [Fact]
+    public void FirstTask_WithTestData_Return95437()
+    {
+        var runner = new Runner07();
+        var result = runner.FirstTask("TestInputFiles/07/TestInput-1.txt");
+        result.Should().Be(95437);
+    }
+}
---
+++ TGC.AdventOfCode2022/Runners/Runner07.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGC.AdventOfCode2022.Abstractions;
+
+namespace TGC.AdventOfCode2022.Runner.Runners;
+
+public class Runner07 : IRunner
+{
+    public bool Accept(int number)
+    {
+        return number == 7;
+    }
+
+    public async Task RunAsync()
+    {
+        Console.WriteLine("Result for Task 1: " + await FirstTask("DataFiles/Input07.txt"));
+    }
+
+    public async Task<int> FirstTask(string inputFile)
+    {
+
+        return 0;
+    }
+}
